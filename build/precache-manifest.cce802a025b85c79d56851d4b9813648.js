9874577f8c0e43b76484f8e7e785dfc819e9aa27
Anurag Mishra <anurag8m@gmail.com>
2019-07-21 12:37:13 +0530

    test

---
+++ build/precache-manifest.cce802a025b85c79d56851d4b9813648.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303627b6adbea82ec17c5380a12dfe32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "016aee8d7404cd3f9fe1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b5fd2c7dcd57941d6fd8",
+    "url": "/static/css/main.8edbbf7e.chunk.css"
+  },
+  {
+    "revision": "016aee8d7404cd3f9fe1",
+    "url": "/static/js/2.dfc3e954.chunk.js"
+  },
+  {
+    "revision": "b5fd2c7dcd57941d6fd8",
+    "url": "/static/js/main.b861e306.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50a353c1ed92087c618ef2b7ca082c2f",
+    "url": "/static/media/boy.50a353c1.png"
+  },
+  {
+    "revision": "36b55d0b324a3574a0f1e8cd51a08f2a",
+    "url": "/static/media/cd.36b55d0b.png"
+  }
+]);
